78dd70b060cc5ddad508be56f8b9b7eee39b52cb
MichaelIV <michaeldowlingiv@gmail.com>
2017-03-04 21:53:31 -0500

    this was our first day of python

---
+++ py/io-classtime.py
@@ -0,0 +1,9 @@
+invar = input("What do you like?\n")
+while invar != "bye":
+	if invar == "music":
+		print("Do you like vaporwave?")
+	elif invar == "anime":
+		print("weeb")
+	else:
+		print("lame")
+	invar = input("What else?\n")
